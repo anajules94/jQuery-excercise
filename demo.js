4ea1c7e61b584552870b00f632f36cecf73d045b
anajules94 <32312980+anajules94@users.noreply.github.com>
2017-10-24 12:21:41 +0200

    done

---
+++ demo.js
@@ -0,0 +1,11 @@
+function init() {
+
+$("#menu ul li:first").addClass("selected");
+$("section article:nth-child(2)").addClass("middle");
+$("#list-container .list-item:odd").addClass("highlighted");
+//4to 
+};
+
+$(document).ready(function() {
+  init();
+});
